b903c562eeda70b6c4876f580d707d934cae5bcb
Rafael Correa <68115778+rafaelccorrea@users.noreply.github.com>
2021-04-29 17:54:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -26,13 +26,56 @@ app.use(express.static('public'));
 
 //Rotas
 app.get('/', (req, res) =>{
-    res.render("index")
+    ArtigosModels.findAll({
+        order:[
+            ['id', 'DESC']
+        ]
+    }).then((articles)=>{
+
+        CategoriaModels.findAll().then((categories)=>{
+            res.render('index', {articles: articles, categories: categories});
+        })
+    })
+})
+
+app.get('/:slug', (req, res) =>{
+    let slug = req.params.slug;
+
+    ArtigosModels.findOne({where: {slug: slug}}).then((article)=>{
+        if(article != undefined){
+            CategoriaModels.findAll().then((categories)=>{
+                res.render('article', {article: article, categories: categories});
+            })
+        }else{
+            res.redirect("/")
+        }
+    }).catch((err)=>{
+        res.redirect("/")
+    })
+})
+
+app.get('/categoria/:slug', (req, res)=>{
+    let slug = req.params.slug;
+    CategoriaModels.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: ArtigosModels}]
+    }).then((category)=>{
+        if(category !== undefined){
+            CategoriaModels.findAll().then((categories)=>{
+                res.render('index', {articles: category.articles, categories: categories});
+            });
+        }else{
+            res.redirect("/")
+        }
+    }).catch((err)=>{
+        res.redirect("/")
+    })
 })
 
 app.use('/', Categorias);
 app.use('/', Artigos);
-
-
 
 
 
